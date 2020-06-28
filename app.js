af169007e7646ef5c8581749187bebf8bbff6702
Guilherme dos Santos Rebelo <guilherme.rebelo@betha.com.br>
2020-06-28 16:08:11 -0300

    ajuste de código

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/lives-hoje', liveHoje);
+app.use('/', liveHoje);
 
 app.listen(3333);
