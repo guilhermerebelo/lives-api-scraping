1878cccd62847468b491da4df40068cb917be375
Guilherme dos Santos Rebelo <guilherme.rebelo@betha.com.br>
2020-06-28 16:04:59 -0300

    ajuste de código

--- app.js
+++ app.js
@@ -3,16 +3,13 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var liveHoje = require('./routes/live-hoje');
-var liveProximaSemana = require('./routes/live-proxima-semana');
 
 var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 
 app.use('/lives-hoje', liveHoje);
-app.use('/lives-proxima-semana', liveProximaSemana); //tem que fazer
 
 app.listen(3333);
